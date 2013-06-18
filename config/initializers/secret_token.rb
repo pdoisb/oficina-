4d289b1cd0bacbc630895038836668bd09057630
lab-cord <lab-cord@ama.(none)>
2013-06-18 11:43:45 -0300

    inicio projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Manha::Application.config.secret_token = '4794fa526f0cf6253133fd5112251a56b7287cfa45e104fbc49b3a55a800d9eaf7bdee959fa27ff6d23b7c43976a216a5ba7314d8ae058b683795b052faf5938'
